0b12ea0f280a1dc81f5bc747d41500d1346803ed
typeng <typeng@live.cn>
2022-05-12 11:49:29 +0800

    初始化购物车页面

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,6 +4,7 @@ import Login from '@/pages/Login'
 import Register from '@/pages/Register'
 import Detail from '@/pages/Detail'
 import AddCartSuccess from '@/pages/AddCartSuccess'
+import ShopCart from '@/pages/ShopCart'
 export default [
   {
     path: '/home',
@@ -46,12 +47,25 @@ export default [
   },
   {
     path: '/detail/:skuId',
-    component: Detail
+    component: Detail,
+    meta: {
+      show: true
+    }
   },
   {
     path: '/addcartsuccess',
     name: 'addcartsuccess',
-    component: AddCartSuccess
+    component: AddCartSuccess,
+    meta: {
+      show: true
+    }
+  },
+  {
+    path: '/shopcart',
+    component: ShopCart,
+    meta: {
+      show: true
+    }
   },
   // 重定向
   {
